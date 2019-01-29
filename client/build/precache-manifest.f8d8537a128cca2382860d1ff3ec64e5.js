0a7be42e5f8aa11575582b117cdfbcc2c203a7c3
Charlie <charlie.islas@gmail.com>
2019-01-29 13:56:02 -0600

    with working routes

---
+++ client/build/precache-manifest.f8d8537a128cca2382860d1ff3ec64e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "67631e3e6e1c4a048523",
+    "url": "/static/js/main.67631e3e.chunk.js"
+  },
+  {
+    "revision": "a294260ab0a7ac89a03f",
+    "url": "/static/js/1.a294260a.chunk.js"
+  },
+  {
+    "revision": "67631e3e6e1c4a048523",
+    "url": "/static/css/main.744a531b.chunk.css"
+  },
+  {
+    "revision": "3221ceb79c52d3c8c521a3ccff24ea05",
+    "url": "/index.html"
+  }
+];
